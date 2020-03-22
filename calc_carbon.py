c99deb53b089e4e3fef26679cc13637230f41c6e
Nila <annadurai@college.harvard.edu>
2020-03-22 19:44:29 -0400

    calculated carbon emissions

--- calc_carbon.py
+++ calc_carbon.py
@@ -1,12 +1,35 @@
 import pandas as pd
 import numpy as np
 from categorize import categorize
+import copy
 
 excel_file = 'CFP_Calculator.xlsx'
 
+
 def calc_carbon():
+	emission_factors = pd.read_excel(excel_file, sheet_name=5)
+	# emission_factors = emission_factors.pivot_table(index = ['Food']) 
 	food_input =  categorize()
-	print(food_input.head())
+
+	emission_numbers = dict((emission_factors['Food'][el],(emission_factors['Emissions'][el],
+		emission_factors['Land Use'][el],emission_factors['Carbon Oppurtunity Costs'][el])) 
+		for el in range(emission_factors.shape[0]))
+
+
+	food_input['Carbon Emissions'] = ''
+	food_input['Land Use'] = ''
+	food_input['Carbon Oppurtunity Costs'] = ''
+
+	for i in range(food_input.shape[0]):
+		food_input.at[i, 'Carbon Emissions'] = (food_input.at[i, 'Units'])* \
+		(emission_numbers.get(food_input.at[i, 'Carbon Category'])[0]/1000)
+
+		food_input.at[i, 'Land Use'] = (food_input.at[i, 'Units'])* \
+		(emission_numbers.get(food_input.at[i, 'Carbon Category'])[1]/10000)
+
+		food_input.at[i, 'Carbon Oppurtunity Costs'] = (food_input.at[i, 'Units'])* \
+		(emission_numbers.get(food_input.at[i, 'Carbon Category'])[2]/1000)
+	
 
 
 calc_carbon()
--- categorize.py
+++ categorize.py
@@ -2,11 +2,6 @@ import pandas as pd
 import numpy as np
 
 excel_file = 'Data2018_19SY.xls'
-categories = np.array(['Beef', 'Pork', 'Poultry', 'Fish', 'Shellfish', 'LambSheepGoat', 'Milk', 
-	'Yogurt', 'Cream', 'Cheese', 'Butter', 'IceCream', 'Eggs', 'Beans', 'Peanuts', 'PeanutButter', 
-	'Soybeans', 'NutsSeeds', 'NutSeedButter', 'Rice', 'Wheat', 'Corn', 'Bread', 'Pasta', 
-	'OtherGrains', 'AlmondMilk', 'CoconutMilk', 'SoyMilk', 'MeatSub', 'CheeseSub', 'EggSub', 'FishSub', 
-	'Fruits', 'Vegetables', 'Roots', 'Sugars', 'VegOils', 'TeaCoffeeSpices', 'Alcohol'])
 
 #milk and cream, preserves
 def categorize():
@@ -16,12 +11,12 @@ def categorize():
 	#reset indices
 	food_input.reset_index(inplace=True)
 	#only include relevant columns
-	food_input = food_input[['Item Name', 'Cost Category Name', 'Purchase Unit']]
+	food_input = food_input[['Item Name', 'Cost Category Name', 'Purchase Unit', 'Units']]
 
 	food_input['Carbon Category'] = ''
 
 	food_input['Carbon Category'] = np.where((food_input['Cost Category Name'] == 'BAKERY - BREADS'), 
-		'Bread', food_input['Carbon Category'])
+		'Wheat', food_input['Carbon Category'])
 
 	food_input['Carbon Category'] = np.where((food_input['Cost Category Name'] == 'BUTTER & MARGARINE'), 
 		'Butter', food_input['Carbon Category'])
@@ -36,19 +31,19 @@ def categorize():
 		'Vegetables', food_input['Carbon Category'])
 
 	food_input['Carbon Category'] = np.where((food_input['Cost Category Name'] == 'CANNED FRUIT'), 
-		'Fruit', food_input['Carbon Category'])
+		'Fruits', food_input['Carbon Category'])
 
 	food_input['Carbon Category'] = np.where((food_input['Cost Category Name'] == 'FROZEN FRUIT'), 
-		'Fruit', food_input['Carbon Category'])
+		'Fruits', food_input['Carbon Category'])
 
 	food_input['Carbon Category'] = np.where((food_input['Cost Category Name'] == 'FRESH FRUIT'), 
-		'Fruit', food_input['Carbon Category'])
+		'Fruits', food_input['Carbon Category'])
 
 	food_input['Carbon Category'] = np.where((food_input['Cost Category Name'] == 'ICE CREAM'), 
-		'IceCream', food_input['Carbon Category'])
+		'Ice Cream', food_input['Carbon Category'])
 
 	food_input['Carbon Category'] = np.where((food_input['Cost Category Name'] == 'PASTA'), 
-		'Pasta', food_input['Carbon Category'])
+		'Wheat', food_input['Carbon Category'])
 
 	food_input['Carbon Category'] = np.where((food_input['Cost Category Name'] == 'POULTRY'), 
 		'Poultry', food_input['Carbon Category'])
@@ -60,16 +55,16 @@ def categorize():
 		'Eggs', food_input['Carbon Category'])
 
 	food_input['Carbon Category'] = np.where(food_input['Cost Category Name'].str.contains
-			("Beer", case=False, na=False), 'Alcohol', food_input['Carbon Category'])
+			("Beer", case=False, na=False), 'Beer', food_input['Carbon Category'])
 
 	food_input['Carbon Category'] = np.where(food_input['Cost Category Name'].str.contains
-			("WINE", case=False, na=False), 'Alcohol', food_input['Carbon Category'])
+			("WINE", case=False, na=False), 'Wine', food_input['Carbon Category'])
 
 	food_input['Carbon Category'] = np.where((food_input['Item Name'].str.contains("TOFU", case=False, na=False) 
-		& (food_input['Cost Category Name'] == 'CHEESE YOGURT & TOFU')), 'TOFU', food_input['Carbon Category'])
+		& (food_input['Cost Category Name'] == 'CHEESE YOGURT & TOFU')), 'Tofu', food_input['Carbon Category'])
 
 	food_input['Carbon Category'] = np.where((food_input['Item Name'].str.contains("CHEESE", case=False, na=False)
-		& (food_input['Cost Category Name'] == 'CHEESE YOGURT & TOFU')), 'CHEESE', food_input['Carbon Category'])
+		& (food_input['Cost Category Name'] == 'CHEESE YOGURT & TOFU')), 'Cheese', food_input['Carbon Category'])
 
 	food_input['Carbon Category'] = np.where((food_input['Cost Category Name'] == 'FISH'), 
 		'Fish', food_input['Carbon Category'])
@@ -89,7 +84,7 @@ def categorize():
 
 	food_input['Carbon Category'] = np.where((food_input['Item Name'].str.contains
 		("lamb", case=False, na=False) 
-		& (food_input['Cost Category Name'] == 'MEAT')), 'LambSheepGoat', food_input['Carbon Category'])
+		& (food_input['Cost Category Name'] == 'MEAT')), 'Lamb', food_input['Carbon Category'])
 	
 	food_input = food_input.loc[food_input['Carbon Category'] != '']
 	#reset indices
